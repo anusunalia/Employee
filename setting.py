06b086d153c3cc9961573ff7cfcceef735fe780d
anusunalia <46678774+anusunalia@users.noreply.github.com>
2019-01-24 13:08:29 +0530

    Add files via upload

--- setting.py
+++ setting.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
 app= Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:\\Users\Anu\Desktop\Employee\database.db'
+file_path = os.path.abspath(os.getcwd())+"\database.db"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+#app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:\\Users\Admin\Desktop\Employee-master\database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
   
